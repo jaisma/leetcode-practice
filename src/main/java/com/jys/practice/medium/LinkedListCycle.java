9489c54c28eb5529efb2941790be44fff8f905b6
Jai Sun <sunjy0410@gmail.com>
2017-10-17 21:23:53 -0500

    more practice!
    it message for your changes. Lines starting

--- src/main/java/com/jys/practice/medium/LinkedListCycle.java
+++ src/main/java/com/jys/practice/medium/LinkedListCycle.java
@@ -19,10 +19,37 @@ public class LinkedListCycle {
 	}
 	
 	public ListNode<Integer> hasCycle(ListNode<Integer> head) {
+	
 		ListNode<Integer> sentinel = new ListNode<>(-1);
+		ListNode<Integer> slow = head;
+		ListNode<Integer> fast = head;
 		
-		
-		
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			// found cycle
+			if (slow == fast) {
+				int cycleLength = 0;
+				do {
+					++cycleLength;
+					fast = fast.next;
+				} while (slow != fast);
+				
+				ListNode<Integer> cycleLenIter = head;
+				while (cycleLength-- > 0) {
+					cycleLenIter = cycleLenIter.next;
+				}
+				
+				ListNode<Integer> iter = head;
+				while (iter != cycleLenIter) {
+					iter = iter.next;
+					cycleLenIter = cycleLenIter.next;
+				}
+				return iter;
+				
+			}
+		}
 		
 		return null;
 	}
