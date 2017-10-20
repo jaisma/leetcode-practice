9f87f9ac0d9d3be11bb2b58de34c0b1aea031403
Sun <js044486@m1515185.northamerica.cerner.net>
2017-10-20 13:56:04 -0500

    wieee

--- src/main/java/com/jys/practice/medium/LinkedListCycle.java
+++ src/main/java/com/jys/practice/medium/LinkedListCycle.java
@@ -1,9 +1,7 @@
 package com.jys.practice.medium;
 
-import com.jys.practice.easy.MergeTwoSortedLists.ListNode;
-
 public class LinkedListCycle {
-	
+
 	public class ListNode<T> {
 		public T val;
 		public ListNode<T> next;
@@ -17,13 +15,10 @@ public class LinkedListCycle {
 			this.next = next;
 		}
 	}
-	
+
 	public ListNode<Integer> hasCycle(ListNode<Integer> head) {
 		ListNode<Integer> sentinel = new ListNode<>(-1);
-		
-		
-		
-		
+
 		return null;
 	}
 
